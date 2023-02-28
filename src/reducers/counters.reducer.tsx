23519112b25e05e33db44a7c3ccf428ad863f9b6
Liam K <liamzlyii@gmail.com>
2023-02-28 11:59:27 -0600

    Fix Counter Component

--- src/reducers/counters.reducer.tsx
+++ src/reducers/counters.reducer.tsx
@@ -2,6 +2,9 @@
 import { configureStore } from '@reduxjs/toolkit';
 import counterReducer from '../Component/Counter/counterSlice';
 
+// this file didnt use!!!
+// index.tsx ->  <Provider store={store}>
+// change to root.reducer !
 
 // Redux Steps 1: configureStore从 Redux Toolkit导入API
 // 这将创建一个 Redux Store，并自动配置(configure) Redux DevTools 扩展，以便您可以在开发时检查商店。
