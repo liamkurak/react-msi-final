b5f6b5a66129fb9cc94b1c193a9d92b37d6d58b1
Liam K <liamzlyii@gmail.com>
2023-03-01 18:51:03 -0600

    fix case when Cart is empty

--- src/Component/home/cart/Cart.tsx
+++ src/Component/home/cart/Cart.tsx
@@ -14,41 +14,70 @@ const Cart = () => {
     const item = Data[+window.location.pathname[6]-1];
 
     const count = useSelector(selectCount);
-    const sum = +(item.price) * count;
+    const sum = +(item?.price) * count;
 
-    return(
+    if(item === undefined){
+        return (
             <>
                 <h1 className='text-center'>YOUR CART </h1>
                 <h5> Product</h5>
-            <div className={css.cart_wrapper} >
-                        <div className={css.wrapper} >
-
-                            {/*========= 111111111 =============*/}
-                            <div>
-                                <img className="card-img-top text-center" src={item.img}/>
-
-                            </div>
-
-                            {/*========== 222222222 ==========*/}
-                            <div className='text-center'>
-                                <h5><b>{item.name}</b></h5>
-                                <p><b>{item.category} Food</b></p>
-                                {/*TODO: Reviews */}
-                                <p>Reviews ☆☆☆</p>
-                                <p>${item.price}</p>
-                                <p>{item.desc}</p>
-                                <p><b>Package Size:</b> {item.weight}</p>
-                                {/*TODO: AddToCart*/}
-                                <Counter/>
-
-                            </div>
+                <div className={css.cart_wrapper} >
+                    <div className={css.wrapper} >
+                        {/*========= 111111111 =============*/}
+                        <div>
+                            <img className="card-img-top text-center" src={'https://static.vecteezy.com/system/resources/previews/005/006/007/original/no-item-in-the-shopping-cart-click-to-go-shopping-now-concept-illustration-flat-design-eps10-modern-graphic-element-for-landing-page-empty-state-ui-infographic-icon-vector.jpg'}
+                            />
                         </div>
+                        <div> <h1> Oops! Your Cart is empty</h1> </div>
+                    </div>
 
                     {/*========== Summary =========*/}
+                    <div>
+                        <p><b>ORDER SUMMERY</b></p>
+                        <h5><b>Subtotal:</b> $ 0 </h5>
+                        <Link to={`${appConstants.shopRoute}`}>
+                            <button className={styles.CHECKOUT}> Continue Shopping </button>
+                        </Link>
+                    </div>
+
+                </div>
+            </>
+        );
+    }
+
+    return(
+        <>
+            <h1 className='text-center'>YOUR CART </h1>
+            <h5> Product</h5>
+            <div className={css.cart_wrapper} >
+                <div className={css.wrapper} >
+
+                    {/*========= 111111111 =============*/}
+                    <div>
+                        <img className="card-img-top text-center" src={item?.img}/>
+
+                    </div>
+
+                    {/*========== 222222222 ==========*/}
+                    <div className='text-center'>
+                        <h5><b>{item?.name}</b></h5>
+                        <p><b>{item?.category} Food</b></p>
+                        {/*TODO: Reviews */}
+                        <p>Reviews ☆☆☆</p>
+                        <p>${item?.price}</p>
+                        <p>{item?.desc}</p>
+                        <p><b>Package Size:</b> {item?.weight}</p>
+                        {/*TODO: AddToCart*/}
+                        <Counter/>
+
+                    </div>
+                </div>
+
+                {/*========== Summary =========*/}
                 <div>
                     <p><b>ORDER SUMMERY</b></p>
 
-                    <h5><b>Items:</b> {item.brand} x {count} </h5>
+                    <h5><b>Items:</b> {item?.brand} x {count} </h5>
                     <h5><b>Subtotal:</b> ${sum.toFixed(2)} </h5>
 
                     <Link to={`${appConstants.checkoutRoute}`}>
@@ -59,7 +88,8 @@ const Cart = () => {
                 </div>
 
             </div>
-            </>
+        </>
+
     );
 }
 
--- src/Component/home/checkout/Checkout.tsx
+++ src/Component/home/checkout/Checkout.tsx
@@ -30,6 +30,79 @@ const Checkout = () => {
                     <p>Total: </p>
                 </span>
 
+                <span>
+                    <form>
+                        <table>
+                            <tr>
+                              <td><label>Name</label></td>
+                              <td>
+                                  {/*<input id="name" type="text" name="name" required>*/}
+                                  <input/>
+                              </td>
+                            </tr>
+
+                            <tr>
+                              <td><label>Card type</label></td>
+                              <td><select name="category">
+                                  <option value="Per">Credit Card</option>
+                                  <option value="Aca">Debit Card</option>
+                                  {/*<option value="Ind"></option>*/}
+                                  </select>
+                              </td>
+                            </tr>
+
+                            <tr>
+                              <td><label>Card Number</label></td>
+                              <td>
+                                  <input/>
+                              </td>
+
+                            </tr>
+
+                            <tr>
+                              <td><label>Exp Date</label></td>
+                              <td> <input/> </td>
+
+                              <td><label>CVV</label></td>
+                              <td> <input/> </td>
+                            </tr>
+
+                            <tr>
+                              <td><label>Address</label></td>
+                              <td>
+                                  {/*<input id="location" type="text" name="location" required>*/}
+                                  <input/>
+                              </td>
+                            </tr>
+
+                            <tr>
+                              <td><label>Email</label></td>
+                              <td>
+                                  {/*<input id="email" type="text" name="email" required>*/}
+                                  <input/>
+                                  </td>
+                            </tr>
+
+                            <tr>
+                              <td><label>Website</label></td>
+                              <td>
+                                  {/*<input id="website" type="url" name="website" placeholder="https://example.com" pattern="https://.*" required>*/}
+                                  <input/>
+                              </td>
+                            </tr>
+
+                            <tr>
+                              <td></td>
+                                <td>
+                                    <input />
+                                    {/*<input type="submit" value="Submit">*/}
+                                </td>
+                            </tr>
+                          </table>
+                    </form>
+                </span>
+
+
             </div>
         </>
     );
