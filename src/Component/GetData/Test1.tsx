60e1ed3306baa2078fa0e12d5e883321a9918817
Liam K <liamzlyii@gmail.com>
2023-03-06 12:38:31 -0600

    add Login/Register fn

---
+++ src/Component/GetData/Test1.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const regisTest = () =>{
+    return(
+        <>
+        <h1>asd</h1>
+        </>
+    );
+}
+
+export default regisTest();
---
+++ src/Component/login/asd.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+function Register(){
+// const Register = () =>{
+    console.log("asd");
+    return (
+        <div>
+            asd
+            <h1>asd  </h1>
+        </div>
+    );
+}
+
+export default Register;
